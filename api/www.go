3954c34b43f696eae645b77186d95e49b55a9736
RubaXa <k.lebedev@corp.mail.ru>
2017-12-19 15:46:46 +0300

    * ui

--- api/www.go
+++ api/www.go
@@ -1,13 +1,29 @@
 package api
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"strings"
+	"time"
 )
+
+type ClientEnv struct {
+	TS int64 `json:"ts"`
+}
 
 func WwwDashboard(w http.ResponseWriter, r *http.Request) {
 	body, _ := ioutil.ReadFile("./www/index.html")
+	envJson, _ := json.Marshal(ClientEnv{
+		TS: time.Now().Unix() * 1000,
+	})
+	html := strings.Replace(
+		string(body),
+		"{/*env*/}",
+		string(envJson),
+		1,
+	)
 
 	w.Header().Set("Content-Type", "text/html")
-	w.Write(body)
+	w.Write([]byte(html))
 }
